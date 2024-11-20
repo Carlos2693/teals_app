d0fec347e97034332fdfce762e8482aac5864b78
Carlos Buruel <charly.ob.26@gmail.com>
2024-11-20 09:40:59 -0600

    [BOc] Add env library

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:teals_app/screen/main_screen.dart';
 
-void main() {
+void main() async {
+
+  await dotenv.load(fileName: '.env');
+
   runApp(const MainApp());
 }
 
